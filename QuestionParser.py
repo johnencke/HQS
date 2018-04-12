b7b7b734f66890f0a9ae2358b2f14565c466d57d
Andy <aaltadonna45@gmail.com>
2018-04-12 14:38:39 -0400

    cleaned up folder

--- QuestionParser.py
+++ QuestionParser.py
@@ -8,25 +8,23 @@ import os
 from PIL import Image
 import pytesseract
 
-def parseText(fileName):
-	text = pytesseract.image_to_string(Image.open('HQ4.png'))
-	text = text.split('\n')
-	question = ''
-	answers = []
-	for i in range(0, len(text)):
-		if text[i] == '' and question == '':
-			question = " ".join(text[0:i])
-		else:
-			if text[i] != '' and question != '':
-				answers.append(text[i])
+def parseText():
+    text = pytesseract.image_to_string(Image.open('HQ4.png'))
+    text = text.split('\n')
+    question = ''
+    answers = []
+    for i in range(0, len(text)):
+        if text[i] == '' and question == '':
+            question = " ".join(text[0:i])
+        else:
+            if text[i] != '' and question != '':
+                answers.append(text[i])
 
+    print('Question: ', question)
+    print('Answer 1: ', answers[0])
+    print('Answer 2: ', answers[1])
+    print('Answer 3: ', answers[2])
 
-	#os.remove('HQ3.png')
-	print('Question: ', question)
-	print('Answer 1: ', answers[0])
-	print('Answer 2: ', answers[1])
-	print('Answer 3: ', answers[2])
-
-parseText("")
+parseText()
 
 
