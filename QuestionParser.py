865adf28f435532b6357bc43ca3b0020386a63d0
John Encke <johnencke@gmail.com>
2018-04-13 01:47:03 -0400

    Added a rudementary click and drag screenshot tool

--- QuestionParser.py
+++ QuestionParser.py
@@ -5,11 +5,17 @@
 '''
 #import cv2
 import os
-from PIL import Image
 import pytesseract
+import pyscreenshot as ImageGrab
+from pynput.mouse import Listener
+from PIL import Image
+import time
+import queue
 
-def parseText(fileName):
-	text = pytesseract.image_to_string(Image.open('HQ4.png'))
+
+
+def parseText(im):
+	text = pytesseract.image_to_string(im)
 	text = text.split('\n')
 	question = ''
 	answers = []
@@ -27,6 +33,43 @@ def parseText(fileName):
 	print('Answer 2: ', answers[1])
 	print('Answer 3: ', answers[2])
 
-parseText("")
+'''
+Takes mouse click 
+returns x and y
+'''
+
+x1 = 1
+y1 = 1
+x2 = 2
+y2 = 2 
+q = queue.Queue(4)
+def captureImage():
+	def on_click(x, y, button, pressed):
+		print('{0} at {1}'.format('Pressed' if pressed else 'Released', (x, y)))
+		if pressed:
+			q.put(x)
+			q.put(y)
+			return True
+		else:
+			q.put(x)
+			q.put(y)
+			return False
+	with Listener(on_click=on_click) as listener:
+		q.join()
+		listener.join()
+	x1 = q.get()
+	y1 = q.get()
+	x2 = q.get()
+	y2 = q.get()
+	im = ImageGrab.grab(bbox=(x1,y1,x2,y2))
+	parseText(im)
+	
+
+
+
+
+
+if __name__ == '__main__':
+	captureImage()
 
 
