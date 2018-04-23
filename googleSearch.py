d4023a1b702a25b33b6d13bd335b3f7fc3bffcdc
Andy <aaltadonna45@gmail.com>
2018-04-23 00:27:51 -0400

    added webbrowser component from rob

--- googleSearch.py
+++ googleSearch.py
@@ -3,19 +3,23 @@ from bs4 import BeautifulSoup
 import requests
 import questionParser
 from PIL import Image
+import webbrowser
 
 my_api_key = "AIzaSyClRm3OS-OCShRJu6W4FJ_PhpUbDOHTMkQ"
 my_cse_id = "015426465276113101398:etj8c0m8u_u"
 
-def google_search(search_term, api_key, cse_id, **kwargs):
-      service = build("customsearch", "v1", developerKey=api_key)
-      res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
-      return res['items']
+# def google_search(search_term, api_key, cse_id, **kwargs):
+#       service = build("customsearch", "v1", developerKey=api_key)
+#       res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+#       return res['items']
 
 im = Image.open('hq4.png')
 qp = questionParser.QuestionParser(im)
+query = qp.question
+query = query.replace(' ', '+')
+print (query)
+webbrowser.open('https://www.google.com/search?q=' + query)
 
-print (qp.answers)
 # search_string = qp.question
 # results = google_search(search_string, my_api_key,my_cse_id, num=10) 
 
