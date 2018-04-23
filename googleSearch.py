d6837c53e3a8e7dd3b85e2bd83a9a0d7bf336cc9
Andy <aaltadonna45@gmail.com>
2018-04-22 23:37:48 -0400

    updated googleSearch to utilize google's api, and fixed an error in pulling answers into a list

--- googleSearch.py
+++ googleSearch.py
@@ -2,19 +2,26 @@ from googleapiclient.discovery import build
 from bs4 import BeautifulSoup
 import requests
 import questionParser
+from PIL import Image
 
 my_api_key = "AIzaSyClRm3OS-OCShRJu6W4FJ_PhpUbDOHTMkQ"
 my_cse_id = "015426465276113101398:etj8c0m8u_u"
 
-# def google_search(search_term, api_key, cse_id, **kwargs):
-#       service = build("customsearch", "v1", developerKey=api_key)
-#       res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
-#       return res['items']
+def google_search(search_term, api_key, cse_id, **kwargs):
+      service = build("customsearch", "v1", developerKey=api_key)
+      res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+      return res['items']
 
-# results = google_search("reddit",my_api_key,my_cse_id, num=10) 
+im = Image.open('hq4.png')
+qp = questionParser.QuestionParser(im)
+
+print (qp.answers)
+# search_string = qp.question
+# results = google_search(search_string, my_api_key,my_cse_id, num=10) 
 
 # for result in results:
 #       print(result["link"])
+
 
 # response = requests.get('https://en.wikipedia.org/wiki/Soviet_Union')
 # soup = BeautifulSoup(response.text, 'html.parser')
@@ -26,15 +33,17 @@ my_cse_id = "015426465276113101398:etj8c0m8u_u"
 # else:
 # 	print (False)
 
-try:
-    from googlesearch import search
-except ImportError: 
-    print("No module named 'google' found")
+# try:
+#     from googlesearch import search
+# except ImportError: 
+#     print("No module named 'google' found")
  
-def makeQuery(qp):
-	query = qp.question
+# def makeQuery(qp):
+# 	query = qp.question
 
-	for i in search(query, tld="com", num=10, stop=1, pause=2):
-   	 print(i)
+# 	for i in search(query, tld="com", num=10, stop=1, pause=2):
+#    	 print(i)
 
-makeQuery(QuestionParser(Image.open('hq4.png')))
+# im = Image.open('hq4.png')
+
+# makeQuery(questionParser.QuestionParser(im))
