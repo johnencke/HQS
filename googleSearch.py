5e35b3e49073e53af400ce17d0201eabc88a9764
Andy <aaltadonna45@gmail.com>
2018-04-22 23:14:45 -0400

    added api key, cse ID, and web scraping with BS4

---
+++ googleSearch.py
@@ -0,0 +1,26 @@
+from googleapiclient.discovery import build
+from bs4 import BeautifulSoup
+import requests
+
+my_api_key = "AIzaSyClRm3OS-OCShRJu6W4FJ_PhpUbDOHTMkQ"
+my_cse_id = "015426465276113101398:etj8c0m8u_u"
+
+def google_search(search_term, api_key, cse_id, **kwargs):
+      service = build("customsearch", "v1", developerKey=api_key)
+      res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+      return res['items']
+
+results = google_search("reddit",my_api_key,my_cse_id, num=10) 
+
+for result in results:
+      print(result["link"])
+
+response = requests.get('https://en.wikipedia.org/wiki/Soviet_Union')
+soup = BeautifulSoup(response.text, 'html.parser')
+
+page = soup.get_text().encode("utf-8")
+
+if 'Hitler'.encode("utf-8") in page:
+	print (True)
+else:
+	print (False)
